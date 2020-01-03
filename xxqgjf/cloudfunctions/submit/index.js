58117a9d4fd6f1d02870be61a1ac1e8e7f4a0342
nolan57 <545266262@qq.com>
2020-01-03 12:35:24 +0800

    v0.0.1

--- xxqgjf/cloudfunctions/submit/index.js
+++ xxqgjf/cloudfunctions/submit/index.js
@@ -6,7 +6,22 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-
+  const db=wx.cloud.database({
+    env:'ssjs57'
+  })
+  const cl=db.collection('xxqgjf')
+  cl.add({
+    data:{
+      _id: wxContext.OPENID,
+      code: event.code,
+      name: event.name,
+      points: event.points,
+      when: event.when
+    },
+    success:function(res){
+      console.log(res)
+    }
+  })
   return {
     event,
     openid: wxContext.OPENID,
--- xxqgjf/miniprogram/pages/index/index.js
+++ xxqgjf/miniprogram/pages/index/index.js
@@ -3,28 +3,47 @@ const app = getApp()
 
 Page({
   data: {
+    date: '',
+    ph: '',
     inputValue: '',
     btnlable: "submit"
   },
   formSubmit:function(e){
+    
     this.setData({
-      inputValue: e.detail.value.input
+      inputValue: e.detail.value.input,
+      date: new Date()
     })
+
     if((this.data.inputValue==''||this.data.inputValue==0)&&(this.data.btnlable =="submit"||this.data.btnlable !="submited")){
       this.setData({
         btnlable:"can't submit nothing"
       })
       return
     }
-    console.log('form submit: ',this.data.inputValue)
 
+    console.log('form submit: ',this.data.inputValue)
+    wx.cloud.callFunction({
+      name: 'submit',
+      data:{
+        code: "080001",
+        name: "wuqiang",
+        points: this.data.inputValue,
+        when: this.data.date.getFullYear().toString()+'/'+(this.data.date.getMonth()+1).toString()+'/'+this.data.date.getDate().toString()
+      },
+      success:function(res){
+        console.log(res.result.event.when)
+      },
+      fail:console.error
+    })
     if(this.data.btnlable !="submited"){
       this.setData({
-        inputValue:'',
-        btnlable:"submited"
+        inputValue: '',
+        btnlable: "submited"
       })
     }
   },
+  
   setsubmit:function(e){
     this.setData({
       btnlable: "submit"
